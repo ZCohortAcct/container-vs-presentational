e91fe859824115c33cb5c674890144697d50d52a
NickisKnowledge <lifechangingstuff@gmail.com>
2019-07-15 07:34:30 -0500

    removed rendering Todo compo

--- src/containers/todoshow.js
+++ src/containers/todoshow.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
-import ToDo from '../components/todo'
 
 class TodoShow extends Component {
   render() {
-    const todo = this.props.todos
     // debugger
+    const todo = this.props.todos
     return(
       <div>
         <ul>
-          <ToDo content={todo.content} completed={todo.completed} />
-          <p><b>{todo.completed ? 'Great job checking stuff off!' : 'You need to take care of this'}</b></p>
+          <h3>{todo.content}</h3>
+          <p>{todo.completed ? 'Great job checking stuff off!' : 'You need to take care of this'}</p>
         </ul>
       </div>
     )
