3067fc3938d2c43077ffedad4f702a2e97d3a250
NickisKnowledge <lifechangingstuff@gmail.com>
2019-07-15 07:15:08 -0500

    built TodoShow compo

---
+++ src/containers/todoshow.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import ToDo from '../components/todo'
+
+class TodoShow extends Component {
+  render() {
+    const todo = this.props.todos[this.props.match.params.id]
+    // debugger
+    return(
+      <div>
+        <ul>
+          <ToDo content={todo.content} completed={todo.completed} />
+          <p><b>{todo.completed ? 'Great job checking stuff off!' : 'You need to take care of this'}</b></p>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default TodoShow;
